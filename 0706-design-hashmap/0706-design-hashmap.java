330cfaf1e0dc3724743cc3aca979b7dda5f6d08a
Vinita66 <vinitakumari68634@gmail.com>
2026-01-26 23:43:46 +0530

    Time: 19 ms (59.21%), Space: 50.6 MB (76.61%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -0,0 +1,53 @@
+import java.util.LinkedList;
+
+class MyHashMap {
+
+    private static class Node {
+        int key, value;
+        Node(int k, int v) { key = k; value = v; }
+    }
+
+    private LinkedList<Node>[] map;
+
+    public MyHashMap() {
+        map = new LinkedList[1000];
+    }
+    
+    public void put(int key, int value) {
+        int i = key % 1000;
+        if (map[i] == null) map[i] = new LinkedList<>();
+
+        for (Node n : map[i]) {
+            if (n.key == key) {
+                n.value = value;
+                return;
+            }
+        }
+        map[i].add(new Node(key, value));
+    }
+    
+    public int get(int key) {
+        int i = key % 1000;
+        if (map[i] == null) return -1;
+
+        for (Node n : map[i])
+            if (n.key == key) return n.value;
+
+        return -1;
+    }
+    
+    public void remove(int key) {
+        int i = key % 1000;
+        if (map[i] == null) return;
+
+        map[i].removeIf(n -> n.key == key);
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
