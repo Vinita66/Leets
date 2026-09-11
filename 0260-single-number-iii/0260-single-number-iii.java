02bfe56b52fc585b0e56cdd214d390880a8d71c1
Vinita66 <vinitakumari68634@gmail.com>
2026-09-11 23:52:36 +0530

    Time: 5 ms (37.85%), Space: 47.8 MB (76.91%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)){
+                set.remove(num);
+            }else{
+                set.add(num);
+            }
+        }
+        int[] result = new int[set.size()];
+        int i = 0;
+        for (int num : set) {
+             result[i++] = num;
+        }
+        return result;
+    }
+}
